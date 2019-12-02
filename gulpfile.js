c6282ac14d6edc63d4915174f14bc1132e9fe4dd
Maxim <maxim.lomako@icloud.com>
2019-12-02 21:02:04 +0300

    gulp with min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
 gulp.task('hello', function(done) {
   console.log('привет мир!');
@@ -14,4 +16,13 @@ gulp.task('browser-sync', function () {
     }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', function () {
+  gulp.src('./*.css')
+    .pipe(cssmin())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./'));
 });
