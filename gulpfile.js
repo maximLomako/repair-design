5024ed12b38d5f6e2aa1eb314c13cf5a9c855037
Maxim <maxim.lomako@icloud.com>
2019-12-02 21:38:09 +0300

    homewrok 2.0

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+
+gulp.task('hello', function(done) {
+  console.log('привет мир!');
+  done();
+});
+
+// Static server
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', function () {
+  return gulp.src('./*.css')
+    .pipe(cssmin())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./'));
+});
