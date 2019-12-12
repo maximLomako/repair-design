3ed9bf7e2f5a46d366ee3c848342f51eea5b7bd9
Maxim <maxim.lomako@icloud.com>
2019-12-13 02:39:23 +0300

    feat: close modal window by esc and click on outside

--- js/main.js
+++ js/main.js
@@ -1,13 +1,36 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   const modal = document.querySelector('.modal');
+  const modalDialog = document.querySelector('.modal__dialog');
+  const modalClick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+    return false;
+  }
+  modalDialog.addEventListener('click', modalClick);
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible')
+  const showModal = () => {
+    modal.classList.add('modal--visible')
   } 
+  const hideModal = () => {
+    modal.classList.remove('modal--visible')
+  }
+
   modalBtn.forEach(element =>  {
-    element.addEventListener('click', switchModal);
+    element.addEventListener('click', showModal);
   });
 
-  closeBtn.addEventListener('click', switchModal);
+  closeBtn.addEventListener('click', hideModal);
+
+  document.onkeydown = (event) => {
+    if (event.key === 'Escape') {
+      hideModal()
+    }
+  };
+
+  modal.addEventListener('click', (event) => {
+      hideModal ()
+  });
 });
+  
